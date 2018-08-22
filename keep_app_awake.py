0a22564d905dc073b9ad3bf67d6337082c511ddc
viviannncui <viviannn@umich.edu>
2018-08-22 17:04:43 -0400

    about keep app awake

---
+++ keep_app_awake.py
@@ -0,0 +1,10 @@
+Some tools can make heroku app automatically awake:
+
+1. http://kaffeine.herokuapp.com
+2. https://uptimerobot.com
+3. https://www.pingdom.com
+
+Articles about this:
+https://quickleft.com/blog/6-easy-ways-to-prevent-your-heroku-node-app-from-sleeping/
+https://devcenter.heroku.com/articles/dynos
+https://devcenter.heroku.com/articles/one-off-dynos
